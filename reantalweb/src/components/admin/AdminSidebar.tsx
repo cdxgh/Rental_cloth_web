67a602c13bd1177a93e6fa14d266b0a1cfa8fba2
cdxgh <khanarshu0786@gmail.com>
2024-05-10 09:00:17 +0530

    update and add files

--- reantalweb/src/components/admin/AdminSidebar.tsx
+++ reantalweb/src/components/admin/AdminSidebar.tsx
@@ -1,187 +1,120 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
-import { AiFillFileText } from "react-icons/ai";
+import { VscError } from "react-icons/vsc";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import CartItemCard from "../components/cart-item";
 import {
-  FaChartBar,
-  FaChartLine,
-  FaChartPie,
-  FaGamepad,
-  FaStopwatch,
-} from "react-icons/fa";
-import { HiMenuAlt4 } from "react-icons/hi";
-import { IoIosPeople } from "react-icons/io";
-import {
-  RiCoupon3Fill,
-  RiDashboardFill,
-  RiShoppingBag3Fill,
-} from "react-icons/ri";
-import { Link, Location, useLocation } from "react-router-dom";
-import { IconType } from "react-icons";
+  addToCart,
+  calculatePrice,
+  discountApplied,
+  removeCartItem,
+} from "../redux/reducer/cartReducer";
+import { RootState, server } from "../redux/store";
+import { CartItem } from "../types/types";
 
-const AdminSidebar = () => {
-  const location = useLocation();
+const Cart = () => {
+  const { cartItems, subtotal, tax, total, shippingCharges, discount } =
+    useSelector((state: RootState) => state.cartReducer);
+  const dispatch = useDispatch();
 
-  const [showModal, setShowModal] = useState<boolean>(false);
-  const [phoneActive, setPhoneActive] = useState<boolean>(
-    window.innerWidth < 1100
-  );
+  const [couponCode, setCouponCode] = useState<string>("");
+  const [isValidCouponCode, setIsValidCouponCode] = useState<boolean>(false);
 
-  const resizeHandler = () => {
-    setPhoneActive(window.innerWidth < 1100);
+  const incrementHandler = (cartItem: CartItem) => {
+    if (cartItem.quantity >= cartItem.stock) return;
+
+    dispatch(addToCart({ ...cartItem, quantity: cartItem.quantity + 1 }));
   };
+  const decrementHandler = (cartItem: CartItem) => {
+    if (cartItem.quantity <= 1) return;
 
+    dispatch(addToCart({ ...cartItem, quantity: cartItem.quantity - 1 }));
+  };
+  const removeHandler = (productId: string) => {
+    dispatch(removeCartItem(productId));
+  };
   useEffect(() => {
-    window.addEventListener("resize", resizeHandler);
+    const { token: cancelToken, cancel } = axios.CancelToken.source();
+
+    const timeOutID = setTimeout(() => {
+      axios
+        .get(`${server}/api/v1/payment/discount?coupon=${couponCode}`, {
+          cancelToken,
+        })
+        .then((res) => {
+          dispatch(discountApplied(res.data.discount));
+          setIsValidCouponCode(true);
+          dispatch(calculatePrice());
+        })
+        .catch(() => {
+          dispatch(discountApplied(0));
+          setIsValidCouponCode(false);
+          dispatch(calculatePrice());
+        });
+    }, 1000);
 
     return () => {
-      window.removeEventListener("resize", resizeHandler);
+      clearTimeout(timeOutID);
+      cancel();
+      setIsValidCouponCode(false);
     };
-  }, []);
+  }, [couponCode]);
+
+  useEffect(() => {
+    dispatch(calculatePrice());
+  }, [cartItems]);
 
   return (
-    <>
-      {phoneActive && (
-        <button id="hamburger" onClick={() => setShowModal(true)}>
-          <HiMenuAlt4 />
-        </button>
-      )}
-
-      <aside
-        style={
-          phoneActive
-            ? {
-                width: "20rem",
-                height: "100vh",
-                position: "fixed",
-                top: 0,
-                left: showModal ? "0" : "-20rem",
-                transition: "all 0.5s",
-              }
-            : {}
-        }
-      >
-        <h2>Logo.</h2>
-        <DivOne location={location} />
-        <DivTwo location={location} />
-        <DivThree location={location} />
-
-        {phoneActive && (
-          <button id="close-sidebar" onClick={() => setShowModal(false)}>
-            Close
-          </button>
+    <div className="cart">
+      <main>
+        {cartItems.length > 0 ? (
+          cartItems.map((i, idx) => (
+            <CartItemCard
+              incrementHandler={incrementHandler}
+              decrementHandler={decrementHandler}
+              removeHandler={removeHandler}
+              key={idx}
+              cartItem={i}
+            />
+          ))
+        ) : (
+          <h1>No Items Added</h1>
         )}
+      </main>
+      <aside>
+        <p>Subtotal: ₹{subtotal}</p>
+        <p>Shipping Charges: ₹{shippingCharges}</p>
+        <p>Tax: ₹{tax}</p>
+        <p>
+          Discount: <em className="red"> - ₹{discount}</em>
+        </p>
+        <p>
+          <b>Total: ₹{total}</b>
+        </p>
+
+        <input
+          type="text"
+          placeholder="Coupon Code"
+          value={couponCode}
+          onChange={(e) => setCouponCode(e.target.value)}
+        />
+
+        {couponCode &&
+          (isValidCouponCode ? (
+            <span className="green">
+              ₹{discount} off using the <code>{couponCode}</code>
+            </span>
+          ) : (
+            <span className="red">
+              Invalid Coupon <VscError />
+            </span>
+          ))}
+
+        {cartItems.length > 0 && <Link to="/shipping">Checkout</Link>}
       </aside>
-    </>
+    </div>
   );
 };
 
-const DivOne = ({ location }: { location: Location }) => (
-  <div>
-    <h5>Dashboard</h5>
-    <ul>
-      <Li
-        url="/admin/dashboard"
-        text="Dashboard"
-        Icon={RiDashboardFill}
-        location={location}
-      />
-      <Li
-        url="/admin/product"
-        text="Product"
-        Icon={RiShoppingBag3Fill}
-        location={location}
-      />
-      <Li
-        url="/admin/customer"
-        text="Customer"
-        Icon={IoIosPeople}
-        location={location}
-      />
-      <Li
-        url="/admin/transaction"
-        text="Transaction"
-        Icon={AiFillFileText}
-        location={location}
-      />
-    </ul>
-  </div>
-);
-
-const DivTwo = ({ location }: { location: Location }) => (
-  <div>
-    <h5>Charts</h5>
-    <ul>
-      <Li
-        url="/admin/chart/bar"
-        text="Bar"
-        Icon={FaChartBar}
-        location={location}
-      />
-      <Li
-        url="/admin/chart/pie"
-        text="Pie"
-        Icon={FaChartPie}
-        location={location}
-      />
-      <Li
-        url="/admin/chart/line"
-        text="Line"
-        Icon={FaChartLine}
-        location={location}
-      />
-    </ul>
-  </div>
-);
-
-const DivThree = ({ location }: { location: Location }) => (
-  <div>
-    <h5>Apps</h5>
-    <ul>
-      <Li
-        url="/admin/app/stopwatch"
-        text="Stopwatch"
-        Icon={FaStopwatch}
-        location={location}
-      />
-      <Li
-        url="/admin/app/coupon"
-        text="Coupon"
-        Icon={RiCoupon3Fill}
-        location={location}
-      />
-      <Li
-        url="/admin/app/toss"
-        text="Toss"
-        Icon={FaGamepad}
-        location={location}
-      />
-    </ul>
-  </div>
-);
-
-interface LiProps {
-  url: string;
-  text: string;
-  location: Location;
-  Icon: IconType;
-}
-const Li = ({ url, text, location, Icon }: LiProps) => (
-  <li
-    style={{
-      backgroundColor: location.pathname.includes(url)
-        ? "rgba(0,115,255,0.1)"
-        : "white",
-    }}
-  >
-    <Link
-      to={url}
-      style={{
-        color: location.pathname.includes(url) ? "rgb(0,115,255)" : "black",
-      }}
-    >
-      <Icon />
-      {text}
-    </Link>
-  </li>
-);
-
-export default AdminSidebar;
+export default Cart;
--- reantalweb/src/components/header.tsx
+++ reantalweb/src/components/header.tsx
@@ -1,44 +1,75 @@
-import { useState } from "react";
-import { FaSearch, FaShoppingBag, FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import {
+  FaSearch,
+  FaShoppingBag,
+  FaSignInAlt,
+  FaUser,
+  FaSignOutAlt,
+} from "react-icons/fa";
+import { useState } from "react";
+import { User } from "../types/types";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import toast from "react-hot-toast";
 
-const user ={_id: "gdfg", role: "admin"};
+interface PropsType {
+  user: User | null;
+}
 
-const Header = () => {
+const Header = ({ user }: PropsType) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
-    const [isOpen, setIsOpen] = useState<boolean>(false);
-  return( <nav className="header">
-    <Link to={"/"}>Home</Link>
-    <Link to={"/search"}>
-        <FaSearch/>
-    </Link>
-    <Link to={"/cart"}>
-        <FaShoppingBag/>
-    </Link>
-    {user?. _id ?(
+  const logoutHandler = async () => {
+    try {
+      await signOut(auth);
+      toast.success("Sign Out Successfully");
+      setIsOpen(false);
+    } catch (error) {
+      toast.error("Sign Out Fail");
+    }
+  };
+
+  return (
+    <nav className="header">
+      <Link onClick={() => setIsOpen(false)} to={"/"}>
+        HOME
+      </Link>
+      <Link onClick={() => setIsOpen(false)} to={"/search"}>
+        <FaSearch />
+      </Link>
+      <Link onClick={() => setIsOpen(false)} to={"/cart"}>
+        <FaShoppingBag />
+      </Link>
+
+      {user?._id ? (
         <>
-        <button onClick={()=> setIsOpen((prev)=> !prev)}>
-            <FaUser/>
-        </button>
-        <dialog open={isOpen}>
+          <button onClick={() => setIsOpen((prev) => !prev)}>
+            <FaUser />
+          </button>
+          <dialog open={isOpen}>
             <div>
-                {user.role=== "admin"&&(
-                    <Link to="/admin/dashboard">Admin</Link>
-                )}
-                <Link to="/orders">Orders</Link>
-                <button>
-                    <FaSignOutAlt/>
-                </button>
+              {user.role === "admin" && (
+                <Link onClick={() => setIsOpen(false)} to="/admin/dashboard">
+                  Admin
+                </Link>
+              )}
+
+              <Link onClick={() => setIsOpen(false)} to="/orders">
+                Orders
+              </Link>
+              <button onClick={logoutHandler}>
+                <FaSignOutAlt />
+              </button>
             </div>
-        </dialog>
+          </dialog>
         </>
-    ):(
+      ) : (
         <Link to={"/login"}>
-            <FaSignInAlt/>
+          <FaSignInAlt />
         </Link>
-    )}
+      )}
     </nav>
   );
-}
+};
 
 export default Header;
